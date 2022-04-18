9caa172842f9285ad25e69722840d5cb1a882739
Suja-dot <79191046+Suja-dot@users.noreply.github.com>
2022-04-18 13:12:13 +0900

    Create C#_GettingUserInput.cs

---
+++ C#_GettingUserInput.cs
@@ -0,0 +1,9 @@
+static void Main(string[] args)
+{
+  string youName;
+  Console.WriteLine("What is your name?");
+  
+  yourName = Console.ReadLine();
+  
+  Console.WirteLine("Hello {0}", yourName);
+}

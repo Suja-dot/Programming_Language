ff864c1ca496bd272ef958fe3dafb621d004e0e5
Suja-dot <79191046+Suja-dot@users.noreply.github.com>
2022-04-18 13:13:19 +0900

    Update C#_GettingUserInput.cs

--- C#_GettingUserInput.cs
+++ C#_GettingUserInput.cs
@@ -6,4 +6,7 @@ static void Main(string[] args)
   yourName = Console.ReadLine();
   
   Console.WirteLine("Hello {0}", yourName);
+  
+  int age = Convert.ToInt32(Console.ReadLine());
+  Console.WriteLine("You are {0} years old", age);
 }
